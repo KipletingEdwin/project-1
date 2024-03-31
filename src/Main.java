f1c02a46bbd1051894d4007c123d0fe5aa1f1d58
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-31 10:29:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        student s = new student();
+        s.id = 3431;
+        s.age = 17;
+        s.name = "Edwin";
+
+
+        System.out.println("Name: "+ s.name+ "  Age "+s.age+ "   Id Number: " + s.id);
+
+    }
+
+
+
+}
